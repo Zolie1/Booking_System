eca1a65578d5b58472bfed7ad72c3114c1cdb769
Zolie <zolitibane02@gmail.com>
2024-10-05 18:38:27 +0200

    update database

--- jojo_db.sql
+++ jojo_db.sql
@@ -1,1 +1,17 @@
-show databases; 
+select * from clients;
+
+update clients set name="Zolie " where client_id =1 ; 
+insert into clients(name, client_contact, client_type) values 
+("Zaza", "123456789", "child") ; 
+
+delete from clients where client_id=2;
+
+show tables; 
+create table clients(
+client_id int auto_increment primary key not null, 
+name varchar(100) not null, 
+client_contact varchar(200) not null, 
+client_type enum("adult", "child") not null
+ ); 
+ 
+ select * from clients; 
